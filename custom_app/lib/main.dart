c86df49f5c1ca9a9e2675fa9498e07bd6dc4bbb9
RJ <rjsouza1995@gmail.com>
2019-10-14 11:07:49 -0700

    Add Fruits Page

--- custom_app/lib/main.dart
+++ custom_app/lib/main.dart
@@ -4,12 +4,16 @@
 
 import 'package:flutter/material.dart';
 
+final _addedFruits = <String>[];
+final _addedBases = <String>[];
+
 void main() => runApp(MaterialApp(
   title: 'Named Routes Demo',
   initialRoute: '/',
   routes: {
     '/': (context) => MyApp(),
-    '/second': (context) => SelectBase()
+    '/second': (context) => SelectBase(),
+    '/third': (context) => SelectFruit(),
   }
 ));
 
@@ -54,7 +58,7 @@ class SelectBase extends StatelessWidget {
         floatingActionButton: FloatingActionButton (
           backgroundColor: Color.fromRGBO(249, 170, 51, 1.0),
           onPressed: () {
-            Navigator.pushNamed(context, '/second');
+            Navigator.pushNamed(context, '/third');
           },
           tooltip: 'Select Bases',
           child: Icon(Icons.check),
@@ -75,7 +79,6 @@ class BasesState extends State<Bases> {
     );
   }
   final _bases = <String>["Whole Milk", "Soy Milk", "Almond Milk", ];
-  final _addedBases = <String>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildBases(){
@@ -86,12 +89,12 @@ class BasesState extends State<Bases> {
           if (i.isOdd)
             return Divider();
           final index = i ~/ 2;
-          return _buildRow(_bases[index]);
+          return _buildBaseRow(_bases[index]);
         }
     );
   }
 
-  Widget _buildRow(String base) {
+  Widget _buildBaseRow(String base) {
     final bool alreadyAdded = _addedBases.contains(base);
     return ListTile(
       title: Text(
@@ -121,86 +124,84 @@ class Bases extends StatefulWidget {
   BasesState createState() => BasesState();
 }
 
+/*
+ * Select Fruit
+ */
+class SelectFruit extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Shakes',
+      theme: new ThemeData(primaryColor: Color.fromRGBO(74, 101, 114, 1.0)),
+      home: Scaffold(
+        body: Center(
+            child: Fruits()
+        ),
+        floatingActionButton: FloatingActionButton (
+          backgroundColor: Color.fromRGBO(249, 170, 51, 1.0),
+          onPressed: () {
+            Navigator.pushNamed(context, '/second');
+          },
+          tooltip: 'Select Bases',
+          child: Icon(Icons.check),
+        ),
+      ),
+    );
+  }
+}
 
-//class SelectBase extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Custom Shakes',
-//      theme: new ThemeData(primaryColor: Color.fromRGBO(74, 101, 114, 1.0)),
-//      home: Scaffold(
-//        body: Center(
-//            child: Bases()
-//        ),
-//        floatingActionButton: FloatingActionButton (
-//          backgroundColor: Color.fromRGBO(249, 170, 51, 1.0),
-//          onPressed: () {
-//            Navigator.pushNamed(context, '/second');
-//          },
-//          tooltip: 'Select Bases',
-//          child: Icon(Icons.check),
-//        ),
-//      ),
-//    );
-//  }
-//}
-//
-///*
-// * Select Fruit
-// */
-//class FruitsState extends State<Fruits> {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text('Protein Shake Bases'),
-//      ),
-//      body: _buildFruits(),
-//    );
-//  }
-//  final _bases = <String>["Whole Milk", "Soy Milk", "Almond Milk", ];
-//  final _addedBases = <String>[];
-//  final _biggerFont = const TextStyle(fontSize: 18.0);
-//
-//  Widget _buildFruits(){
-//    return ListView.builder(
-//        padding: const EdgeInsets.all(16.0),
-//        itemCount: _bases.length*2,
-//        itemBuilder: (context, i) {
-//          if (i.isOdd)
-//            return Divider();
-//          final index = i ~/ 2;
-//          return _buildFruitRow(_bases[index]);
-//        }
-//    );
-//  }
-//
-//  Widget _buildFruitRow(String base) {
-//    final bool alreadyAdded = _addedBases.contains(base);
-//    return ListTile(
-//        title: Text(
-//          base,
-//          style: _biggerFont,
-//        ),
-//        trailing: Icon(
-//          alreadyAdded ? Icons.add_circle : Icons.add_circle_outline,
-//          color: alreadyAdded ? Color.fromRGBO(249, 170, 51, 1.0) : null,
-//        ),
-//        onTap: () {
-//          setState(() {
-//            if (alreadyAdded) {
-//              _addedBases.remove(base);
-//            }
-//            else {
-//              _addedBases.add(base);
-//            }
-//          });
-//        }
-//    );
-//  }
-//}
-//
-//class Bases extends StatefulWidget {
-//  @override
-//  BasesState createState() => BasesState();
-//}
+class FruitsState extends State<Fruits> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Protein Shake Bases'),
+      ),
+      body: _buildFruits(),
+    );
+  }
+  final _fruits = <String>["Mangos", "Strawberries", "Bananas", ];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildFruits(){
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: _fruits.length*2,
+        itemBuilder: (context, i) {
+          if (i.isOdd)
+            return Divider();
+          final index = i ~/ 2;
+          return _buildFruitRow(_fruits[index]);
+        }
+    );
+  }
+
+  Widget _buildFruitRow(String fruit) {
+    final bool alreadyAdded = _addedFruits.contains(fruit);
+    return ListTile(
+        title: Text(
+          fruit,
+          style: _biggerFont,
+        ),
+        trailing: Icon(
+          alreadyAdded ? Icons.add_circle : Icons.add_circle_outline,
+          color: alreadyAdded ? Color.fromRGBO(249, 170, 51, 1.0) : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadyAdded) {
+              _addedFruits.remove(fruit);
+            }
+            else {
+              _addedFruits.add(fruit);
+            }
+          });
+        }
+    );
+  }
+}
+
+class Fruits extends StatefulWidget {
+  @override
+  FruitsState createState() => FruitsState();
+}
